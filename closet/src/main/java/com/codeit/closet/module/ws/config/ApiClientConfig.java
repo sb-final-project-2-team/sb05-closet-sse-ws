9d84c75458fad1cd4acfc3ec909cc69c5edb8641
5ranke <wjdtnwls0106@gmail.com>
2026-01-02 13:43:21 +0900

    feat: 타임아웃 설정, base url 수정

--- closet/src/main/java/com/codeit/closet/module/ws/config/ApiClientConfig.java
+++ closet/src/main/java/com/codeit/closet/module/ws/config/ApiClientConfig.java
@@ -3,15 +3,24 @@ package com.codeit.closet.module.ws.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class ApiClientConfig {
+
     @Bean
     public RestClient dmApiRestClient(
                                        RestClient.Builder builder,
                                        @Value("${dm.api.base-url}") String baseUrl
     ) {
-        return builder.baseUrl(baseUrl).build();
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(5_000);
+        factory.setReadTimeout(10_000);
+
+        return builder
+                .baseUrl(baseUrl)
+                .requestFactory(factory)
+                .build();
     }
 }
