120d68dcb9b71b8ae761993cac2f6abc3def3e73
backKim1024 <dydgml7313@naver.com>
2026-01-23 10:21:32 +0900

    refactor: NotificationSseMapper 도입 및 카프카 컨슈머 로직 개선

--- closet/src/main/java/com/codeit/closet/module/sse/kafka/NotificationEventConsumer.java
+++ closet/src/main/java/com/codeit/closet/module/sse/kafka/NotificationEventConsumer.java
@@ -3,11 +3,13 @@ package com.codeit.closet.module.sse.kafka;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import com.codeit.closet.common.entity.Notification;
 import com.codeit.closet.module.sse.dto.NotificationCreatedEventDTO;
+import com.codeit.closet.module.sse.mapper.NotificationSseMapper;
 import com.codeit.closet.module.sse.util.SseEmitterManager;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,33 +20,37 @@ public class NotificationEventConsumer {
 
 	private final ObjectMapper objectMapper;
 	private final SseEmitterManager emitterManager;
+	private final NotificationSseMapper notificationSseMapper;
+
+	@PostConstruct
+	public void init() {
+		log.info("[Kafka Consumer] 초기화 완료");
+	}
 
 	@KafkaListener(
 		topics = NotificationTopics.NOTIFICATION_CREATED,
-		groupId = "${spring.kafka.consumer.group-id}"
+		groupId = "notification-group"
 	)
-	public void consume(String payload) {
-		log.info("[Test] raw payload = {}", payload);
+	public void consume(String message) {
+		log.info("[Kafka] 메시지 수신 시작! Message: {}", message);
 
-		try{
-			NotificationCreatedEventDTO event =
-				objectMapper.readValue(payload,NotificationCreatedEventDTO.class);
+		try {
+			NotificationCreatedEventDTO event = objectMapper.readValue(message, NotificationCreatedEventDTO.class);
 
-			log.info("[SSE] 알림 수신, id={}, receiverId={}, level={}, title={}",
-				event.id(), event.receiverId(), event.level(), event.title());
+			Notification notification = notificationSseMapper.toDto(event);
+
+			log.info("[SSE] 알림 성공, id={}, receiverId={}",
+				event.id(), event.receiverId());
 
 			emitterManager.send(
 				event.receiverId(),
-				"notification",
-				event
+				"notifications",
+				notification
 			);
-		} catch (JsonProcessingException e) {
-			log.warn("[Kafka] JSON 파싱 실패");
-		}
-		catch (Exception e) {
+
+			log.info("[SSE] 알림 전송 완료");
+		} catch (Exception e) {
 			log.error("[Kafka] 알 수 없는 오류", e);
 		}
-
-
 	}
 }
